199d19e57bcac6d7b7c7abb1d6977b66c9c756e2
Lars Åslin <lars.aslin@bygglet.com>
2023-06-08 13:35:28 +0200

    Replaced oas files with one consolidatet for Core

--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -3,11 +3,8 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
     urls: [
-        {url: "./customer.oas.json", name: "Customer"},
-        {url: "./company.oas.json", name: "Company"},
-        {url: "./economy.oas.json", name: "Economy"},
+        {url: "./openapi.stage.json", name: "Core"},
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
