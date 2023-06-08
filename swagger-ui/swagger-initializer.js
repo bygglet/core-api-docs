a2f0ffd0d53381a86d7c4f325219c39cd13855da
Lars Åslin <lars.aslin@bygglet.com>
2023-06-08 14:11:19 +0200

    Update oas path and reduce cache ttl

--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-        {url: "./openapi.stage.json", name: "Core"},
+        {url: "./core-oas/openapi.json", name: "Core"},
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
